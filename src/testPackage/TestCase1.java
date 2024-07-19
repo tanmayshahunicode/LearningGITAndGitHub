e13ae01b79867eae2e5830491473aa6d5507d4fc
Tanmay Shah <Tanmay Shah@Tanmay-Shah>
2024-07-19 16:57:33 +0530

    I have modified testcase 1 and added a TestCase2

--- src/testPackage/TestCase1.java
+++ src/testPackage/TestCase1.java
@@ -5,6 +5,8 @@ public class TestCase1 {
 	public static void main(String[] args) {
 
 		System.out.println("Learning GIT and GItHub");
+		
+		System.out.println("Adding a code");
 	}
 
 }
---
+++ src/testPackage/TestCase2.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is a new Test CAse");
+	}
+
+}
